1dbef8a7e914705d2be115b62b13f71b642afd33
vishakahegde <hegde.vishaka@gmail.com>
2020-05-26 23:51:45 +0200

    created a dummy reducer function

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,10 @@
 // src/store/reducer.js
 import { combineReducers } from "redux";
-import someFeatureReducer from "./someFeature/reducer";
+//import someFeatureReducer from "./someFeature/reducer";
+
+function someFeatureReducer(state = "hi", action) {
+  return state;
+}
 
 const reducer = combineReducers({
   someFeature: someFeatureReducer,
