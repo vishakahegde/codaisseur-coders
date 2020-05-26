fd47796854dcc6d5d4551143c57539563d61c4d0
vishakahegde <hegde.vishaka@gmail.com>
2020-05-26 23:53:55 +0200

    moved pages folder to src and coorected App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+// src/App.js
 import React from "react";
-
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
