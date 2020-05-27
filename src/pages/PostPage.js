0de2f911e56c76542206be64b10ab291a74125c5
vishakahegde <hegde.vishaka@gmail.com>
2020-05-28 00:31:33 +0200

    show post and comments

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { fetchPost } from "../store/postPage/actions";
 import { useDispatch, useSelector } from "react-redux";
 import { selectPostAndComments } from "../store/postPage/selectors";
+import moment from "moment";
 
 import ReactMarkdown from "react-markdown";
 
@@ -24,12 +25,41 @@ export default function PostPage() {
       ) : (
         <>
           <h1>{postData.post.title}</h1>
-          <p className="meta">TODO</p>
+          <p className="meta">
+            By <strong>{postData.post.developer.name}</strong> &bull;{" "}
+            {moment(postData.post.createdAt).format("DD-MM-YYYY")} &bull;{" "}
+            {/* {post.post_likes.length} likes &bull;{" "} */}
+            <span className="tags">
+              {postData.post.tags.map((tag) => {
+                return (
+                  <React.Fragment key={tag.id}>
+                    <span className="Tag">{tag.tag}</span>{" "}
+                  </React.Fragment>
+                );
+              })}
+            </span>
+          </p>
 
           <ReactMarkdown source={postData.post.content} />
 
           <h2>Comments</h2>
-          <p>TODO</p>
+          {postData.comments.rows.length === 0 ? (
+            <p>
+              <em>No comments left behind yet :(</em>
+            </p>
+          ) : (
+            postData.comments.rows.map((comment) => {
+              return (
+                <div key={comment.id}>
+                  <p>{comment.text}</p>
+                  <p className="meta">
+                    By <strong>{comment.developer.name}</strong> &bull;{" "}
+                    {moment(comment.createdAt).format("DD-MM-YYYY")}{" "}
+                  </p>
+                </div>
+              );
+            })
+          )}
         </>
       )}
     </div>
