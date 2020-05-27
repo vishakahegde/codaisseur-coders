07aaae7bdc342a31f6b4a715c216d8c84bd03a38
vishakahegde <hegde.vishaka@gmail.com>
2020-05-28 00:17:11 +0200

    correct posts key to post

--- src/store/postPage/selectors.js
+++ src/store/postPage/selectors.js
@@ -2,7 +2,7 @@ export function selectPostAndComments(reduxState) {
   return reduxState.postPage.loading
     ? null
     : {
-        posts: reduxState.postPage.post,
+        post: reduxState.postPage.post,
         comments: reduxState.postPage.comments,
       };
 }
