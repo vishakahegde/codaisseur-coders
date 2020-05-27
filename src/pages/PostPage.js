77d4df0b6afc657b9cd99067a3b3aa1ef57a3dfd
vishakahegde <hegde.vishaka@gmail.com>
2020-05-28 00:05:06 +0200

    import selector selectPostAndComments and show Loading OR Done

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,20 +1,24 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { fetchPost } from "../store/postPage/actions";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { selectPostAndComments } from "../store/postPage/selectors";
 
 export default function PostPage() {
   const { id } = useParams();
+  const data = useSelector(selectPostAndComments);
 
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchPost(id));
   }, [dispatch, id]);
+
   return (
     <div>
       <h1>Post Page</h1>
       <h2>{id}</h2>
+      {data === null ? <h3>Loading...</h3> : <h3>Done!</h3>}
     </div>
   );
 }
