5046271a2e15d5828aa949efca3df7b13b258bc6
vishakahegde <hegde.vishaka@gmail.com>
2020-05-27 22:40:03 +0200

    move fetchNext5Posts from local to thunk function in action.js

--- src/store/feed/action.js
+++ src/store/feed/action.js
@@ -1,3 +1,7 @@
+import axios from "axios";
+
+const API_URL = `https://codaisseur-coders-network.herokuapp.com`;
+
 export function startLoading() {
   return {
     type: "startLoading",
@@ -9,4 +13,18 @@ export function postsFetched(morePosts) {
     type: "postsFetched",
     payload: morePosts,
   };
+}
+
+export async function fetchNext5Posts(dispatch, getState) {
+  //Trigger action for start loading
+  dispatch(startLoading());
+
+  const offset = getState().feed.posts.length;
+
+  const response = await axios.get(`${API_URL}/posts?offset=${offset}&limit=5`);
+
+  const morePosts = response.data.rows;
+
+  // trigger postsFetched action to save data
+  dispatch(postsFetched(morePosts));
 }
