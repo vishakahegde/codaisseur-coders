47c8c2058e2581a644b49fef2f03b8822f988283
vishakahegde <hegde.vishaka@gmail.com>
2020-05-27 22:22:06 +0200

    add more case for reducer

--- src/store/feed/reducer.js
+++ src/store/feed/reducer.js
@@ -1,10 +1,23 @@
 const initialState = {
-  loading: true,
+  loading: false,
   posts: [],
 };
 
 export default function FeedSliceReducer(state = initialState, action) {
   switch (action.type) {
+    case "startLoading": {
+      return {
+        ...state,
+        loading: true,
+      };
+    }
+    case "postsFetched": {
+      return {
+        ...state,
+        loading: false,
+        posts: [...state.posts, ...action.payload],
+      };
+    }
     default: {
       return state;
     }
