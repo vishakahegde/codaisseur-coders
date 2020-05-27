6a5153da757b60e2914e77c222bbd550ca707876
vishakahegde <hegde.vishaka@gmail.com>
2020-05-28 00:42:02 +0200

    Add LoginPage and add route in App

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import PostPage from "./pages/PostPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <div>
       <Switch>
         {/* more pages to be added here later */}
+        <Route path="/login" component={LoginPage} />
         <Route path="/post/:id" component={PostPage} />
         <Route component={HomePage} />
       </Switch>
